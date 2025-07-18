1d2aed88408b924f89057a7f86d1cd2e29ab510c
Sakumo21 <moaud.fdofus@gmail.com>
2025-07-18 20:38:07 +0100

    18/7/25:completed chunked case

--- ziel-hac/main.cpp
+++ ziel-hac/main.cpp
@@ -11,19 +11,14 @@ int parsechunked(request &req, server ser) //<-- i need to parse chuncked body, 
 {
 	std::vector<std::string> chunks = split(req.getBody(), "\r\n");
 	std::string upload_file = ser.getUploadFile() + "/uploads.txt";
-	std::cout << "Upload file: " << upload_file << std::endl;
-	int fd = open(upload_file.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	int fd = open(upload_file.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
 	{
 		std::cerr << "Failed to open upload file." << std::endl;
-		fd = open("/tmp/upload.txt", O_RDONLY | O_CREAT | O_TRUNC, 0644);
+		fd = open("/tmp/upload.txt", O_RDWR | O_CREAT | O_TRUNC, 0644);
 		if (fd < 0)
-		{
-			std::cerr << "Failed to open upload file." << std::endl;
-			return -1; // Error opening file
-		}
+			return (std::cerr << "Failed to open upload file." << std::endl, 0);
 	}
-	std::cout << "fd: " << fd << std::endl;
 	long chunk_size = 0;
 	int i = 0;
 	std::istringstream(chunks[i]) >> std::hex >> chunk_size;
@@ -34,10 +29,11 @@ int parsechunked(request &req, server ser) //<-- i need to parse chuncked body, 
 		i++;
 		std::istringstream(chunks[i]) >> std::hex >> chunk_size;
 	}
-	return close(fd);
+	return 1;
 }
 
-int main() {
+int main() 
+{
 	server serv;
 	request req;
 	int i = parsechunked(req, serv);
