b32ecada886c8dc8763582c701b0d5c56d8a43ee
Sakumo21 <moaud.fdofus@gmail.com>
2025-07-20 20:12:37 +0100

    20/7/25: 95% of post is completed

--- ziel-hac/cgi_utils.cpp
+++ ziel-hac/cgi_utils.cpp
@@ -4,30 +4,32 @@
 #include "cgi_utils.hpp"
 //DO: split function to split a string by a delimiter
 //RETURN: a vector of strings
-std::vector<std::string> split(const std::string& str, const std::string& delimiters)
-{
-    std::vector<std::string> result;
-    std::string token;
-    
-    for (size_t i = 0; i < str.length(); ++i)
-    {
-        if (delimiters.find(str[i]) != std::string::npos)
-        {
-            if (!token.empty())
-            {
-                result.push_back(token);
-                token.clear();
+std::vector<std::string> split(const std::string& str, const std::string& delimiters) {
+    std::vector<std::string> parts;
+    size_t start = 0;
+    size_t pos = 0;
+    while ((pos = str.find(delimiters, start)) != std::string::npos) {
+        if (pos > start) {
+            std::string part = str.substr(start, pos - start);
+            if (!part.empty()) {
+                parts.push_back(part);
             }
         }
-        else
-        {
-            token += str[i];
-        }
+        start = pos + delimiters.length();
     }
-
-    if (!token.empty())
-        result.push_back(token);
-
-    return result;
+    
+    return parts;
 }
 
+
+std::vector<std::string> split_header_and_body(const std::string& str) {
+    std::vector<std::string> parts;
+    size_t pos = str.find("\r\n\r\n");
+    if (pos != std::string::npos) {
+        parts.push_back(str.substr(0, pos)); // Headers
+        parts.push_back(str.substr(pos + 4)); // Body
+    } else {
+        parts.push_back(str); // No body found
+    }
+    return parts;
+}
