9a37cdad740f210ed85068b38a6d97293541af65
sakumo21 <mouad.fdofus@gmail.com>
2025-07-22 15:39:59 +0100

    checking2

--- ziel-hac/cgi_utils.cpp
+++ ziel-hac/cgi_utils.cpp
@@ -26,7 +26,7 @@ std::vector<std::string> split_header_and_body(const std::string& str) {
     std::vector<std::string> parts;
     size_t pos = str.find("\r\n\r\n");
     if (pos != std::string::npos) {
-        parts.push_back(str.substr(0, pos));
+        parts.push_back(str.substr(0, pos));    
         parts.push_back(str.substr(pos + 4));
     } else {
         parts.push_back(str);
